81357bf3648ef700d153eaac0410f9423eba5af9
Panagiotis Papaionnou <papajohn@upatras.gr>
2021-11-16 12:55:20 +0000

    Allow access to server from remote sites

--- server/src/openapi_server/main.py
+++ server/src/openapi_server/main.py
@@ -16,10 +16,25 @@ from openapi_server.apis.events_subscription_api import router as EventsSubscrip
 from openapi_server.apis.notification_listeners__client_side_api import router as NotificationListenersClientSideApiRouter
 from openapi_server.apis.resource_api import router as ResourceApiRouter
 
+
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(
     title="API Resource Inventory Management",
     description="## TMF API Reference: TMF639 - Resource Inventory   ### Release : 19.5 - December 2019  Resource Inventory  API goal is to provide the ability to manage Resources.  ### Operations Resource Inventory API performs the following operations on the resources : - Retrieve an entity or a collection of entities depending on filter criteria - Partial update of an entity (including updating rules) - Create an entity (including default values and creation rules) - Delete an entity (for administration purposes) - Manage notification of events",
     version="4.0.0",
+)
+
+origins = [
+            "*"
+                        ]
+
+app.add_middleware(
+CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(EventsSubscriptionApiRouter)

4f7df30272efa25412980da7ab1a716ad5cc46c0
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-01-21 10:23:10 +0200

    Patch an entry in mongodb

--- server/src/openapi_server/mongo_db.py
+++ server/src/openapi_server/mongo_db.py
@@ -10,7 +10,14 @@ from pymongo.errors import ConnectionFailure
 from pprint import pprint
 
 from openapi_server.models.resource import Resource
+from openapi_server.models.resource_update import ResourceUpdate
 from openapi_server.models.resource import Characteristic
+
+
+from openapi_server.models.resource_administrative_state_type import ResourceAdministrativeStateTypeEnum
+from openapi_server.models.resource_operational_state_type import ResourceOperationalStateTypeEnum
+from openapi_server.models.resource_status_type import ResourceStatusTypeEnum
+from openapi_server.models.resource_usage_state_type import ResourceUsageStateTypeEnum
 
 print("MONGO_DB")
 def connect():
@@ -36,15 +43,11 @@ def connect():
             return mongo_conn
 
     return None
-  
 
-def get_resource(id=None):
-    print("MONGO_DB_GET")
+def checkConnection():
     TARGET_DB="RMS"
     TARGET_COLLECTION="gNodeBs"
-    ret_list = list() #[success = True, UpdatedRecordList] / [success = False, error message]
-    resources_list=list()
-    success = True
+
 
     print("****************************")
     conn=connect()
@@ -55,6 +58,7 @@ def get_resource(id=None):
         print("RMS DB is Missing!")
         success = False
         message = "Error DBM."
+        return None
     print(conn[TARGET_DB].list_collection_names())
     if TARGET_COLLECTION in conn[TARGET_DB].list_collection_names():
         print("COLLECTION in DB is OK!")
@@ -62,20 +66,34 @@ def get_resource(id=None):
         print("COLLECTION in DB is Missing!")
         success = False
         message = "Error CDBM."
+        return None
+    return conn
 
-    target_db=conn[TARGET_DB]
-    target_collection=target_db[TARGET_COLLECTION]
-    resourcesCursor=None
-    #TODO: add try/catch
-    if id is None:
-        resourcesCursor = target_collection.find()
-    else:
-        resourcesCursor = target_collection.find_one({"_id":id})
-    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-    # for document in resourcesCursor:
-    #     print(document)
-    # print(resourcesCursor)
-    # print(type(resourcesCursor))
+
+def get_resource(id=None):
+    print("MONGO_DB_GET")
+    TARGET_DB="RMS"
+    TARGET_COLLECTION="gNodeBs"
+    ret_list = list() #[success = True, UpdatedRecordList] / [success = False, error message]
+    resources_list=list()
+    success = True
+    
+    conn=checkConnection()
+
+    if conn:
+        target_db=conn[TARGET_DB]
+        target_collection=target_db[TARGET_COLLECTION]
+        resourcesCursor=None
+        #TODO: add try/catch
+        if id is None:
+            resourcesCursor = target_collection.find()
+        else:
+            resourcesCursor = target_collection.find_one({"_id":id})
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        # for document in resourcesCursor:
+        #     print(document)
+        # print(resourcesCursor)
+        # print(type(resourcesCursor))
 
     ret_list.append(success)
     ret_list.append(resourcesCursor)
@@ -134,6 +152,99 @@ def insert_resource(newResource:Resource):
             success = False
             message = "Error Code: " + str(err.errno) + " | Message: " + err.msg
     #TODO: MAke this list a dict
+    ret_list.append(success)
+    ret_list.append(message)
+    ret_list.append(return_code)
+    ret_list.append(error_msg)
+    return ret_list
+
+def patch_resource(id:str, patch_resource:ResourceUpdate):
+    print("MONGO_DB_PATCH")
+    TARGET_DB="RMS"
+    TARGET_COLLECTION="gNodeBs"
+    ret_list = list() #[success = True, UpdatedRecordList] / [success = False, error message]
+    success = True
+    message = ""
+    return_code=200
+    error_msg=''
+
+    print("****************************")
+    conn=connect()
+
+    if TARGET_DB in conn.list_database_names():
+        print("RMS DB is OK!")
+    else:
+        print("RMS DB is Missing!")
+        success = False
+        message = "Error DBM."
+    print(conn[TARGET_DB].list_collection_names())
+    if TARGET_COLLECTION in conn[TARGET_DB].list_collection_names():
+        print("COLLECTION in DB is OK!")
+    else:
+        print("COLLECTION in DB is Missing!")
+        success = False
+        message = "Error CDBM."
+
+    target_db=conn[TARGET_DB]
+    target_collection=target_db[TARGET_COLLECTION]
+    if success is True:
+        try:
+            print("PAPA2")
+            #only allow certain fields to be patched
+            print(patch_resource.dict())
+            for key, value in patch_resource.dict().items():
+                if value:
+                    print (key, value)
+                    print (value)
+                    print(ResourceOperationalStateTypeEnum.enable.value)
+                    print(ResourceOperationalStateTypeEnum["enable"].value)
+                    print(ResourceOperationalStateTypeEnum.enable)
+                    print(ResourceOperationalStateTypeEnum["enable"])
+
+                    tar="resource."+key
+                    print(tar)
+                    print(ResourceOperationalStateTypeEnum[value.value].value)
+                    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+                    target_collection.update_one({'_id': id},{"$set": {tar:ResourceOperationalStateTypeEnum[value.value].value}}, upsert=False)
+            
+           # my_entry = { "_id":newResource.name,"resource": newResource.dict() }
+            #target_collection.replace_one({'_id': id},{'_id': id,"resource": patch_resource.dict()}, upsert=True)
+            #target_collection.replace_one({'_id': id},{"DFDFD":"dfdf"}, upsert=True)
+            print("PAPA3")
+        except error as err:
+            print("PAPA3.4")
+            print(err)
+            print("Error Code:", err.errno)
+            print("Message", err.msg)
+            success = False
+            message = "Error Code: " + str(err.errno) + " | Message: " + err.msg
+    #TODO: MAke this list a dict
+    print(success)
+    ret_list.append(success)
+    ret_list.append(message)
+    ret_list.append(return_code)
+    ret_list.append(error_msg)
+    return ret_list
+    return
+    print("PAPA1")
+    if success is True:
+        try:
+            print("PAPA2")
+            print(patch_resource.dict())
+           # my_entry = { "_id":newResource.name,"resource": newResource.dict() }
+            target_collection.replace_one({'_id': id},{'_id': id,"resource": patch_resource.dict()}, upsert=True)
+            #target_collection.replace_one({'_id': id},{"DFDFD":"dfdf"}, upsert=True)
+            print("PAPA3")
+        except error as err:
+            print("PAPA3.4")
+            print(err)
+            print("Error Code:", err.errno)
+            print("Message", err.msg)
+            success = False
+            message = "Error Code: " + str(err.errno) + " | Message: " + err.msg
+    #TODO: MAke this list a dict
+    print(success)
     ret_list.append(success)
     ret_list.append(message)
     ret_list.append(return_code)
